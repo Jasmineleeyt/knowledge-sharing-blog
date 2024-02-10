00793f014de3a913356afbe9cf92e70029f45ab1
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-02-10 18:47:11 -0500

    Added post seeds

--- seeds/userData.js
+++ seeds/userData.js
@@ -2,22 +2,27 @@ const { User } = require('../models');
 
 const userData = [
     {
+        id: 1,
         username: 'Rachel1105',
         password: 'vlJrvDpw&'
     },
     {
+        id: 2,
         username: 'jackiew',
         password: 'cB9sveCPNW'
     },
     {
+        id: 3,
         username: 'johndoe',
         password: 'F^6$Nx6'
     },
     {
+        id: 4,
         username: 'minniey',
         password: 'wOKjNNrGK7'
     },
     {
+        id: 5,
         username: 'janewhite',
         password: 'abcd1234'
     },

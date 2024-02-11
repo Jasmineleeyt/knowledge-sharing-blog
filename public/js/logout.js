8f7a37c637d271be9448d056a2901a3639f390cc
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-02-11 18:47:24 -0500

    Added the frontend logout function

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+const signout = async () => {
+    const response = await fetch('/api/user/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log out. Please try again');
+    }
+  };
+  
+  document.querySelector('#signout').addEventListener('click', signout);
+  
